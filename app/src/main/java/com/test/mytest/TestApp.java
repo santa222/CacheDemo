8fc940b59bde2dcebf7cfe4bb1578361f28c4c49
mika_1990 <santamail222@gmail.com>
2015-03-27 17:59:32 +0800

    fixed spelling mistake

--- app/src/main/java/com/test/mytest/TestApp.java
+++ app/src/main/java/com/test/mytest/TestApp.java
@@ -3,25 +3,25 @@ package com.test.mytest;
 import android.app.Application;
 
 import com.android.volley.RequestQueue;
+import com.test.mytest.network.RequestQueueSingleton;
 
 /**
  * Created by mika_1990 on 14-12-15.
  */
-public class TextApp extends Application {
-    private static TextApp mInstance;
+public class TestApp extends Application {
+    private static TestApp mInstance;
 
     public void onCreate() {
         //mRequestQueue = Volley.newRequestQueue(this);//this is a basic way
         mInstance = this;
+        //init request queque as soon as app create
         RequestQueueSingleton.getInstance(this.getApplicationContext()).getRequestQueue();
     }
 
-    public static synchronized TextApp getInstance() {
+    public static synchronized TestApp getInstance() {
         return mInstance;
     }
 
-    public RequestQueue getRequestQueue(){
-        return RequestQueueSingleton.getInstance(this.getApplicationContext()).getRequestQueue();
-    }
+
 
 }
